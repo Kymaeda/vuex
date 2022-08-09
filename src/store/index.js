e52cf707700eda7eb3fcf274bdbf73295eb3799e
Kymaeda <kyo.m.720@gmail.com>
2022-08-09 20:02:28 +0900

    vuex install & setup
    - install
    ```
    yarn add vuex@next --save
    ```
      - cf. https://vuex.vuejs.org/ja/installation.html
    
    - setup `createStore`
      - cf. https://vuex.vuejs.org/ja/guide/#%E3%82%B7%E3%83%B3%E3%83%97%E3%83%AB%E3%81%AA%E3%82%B9%E3%83%88%E3%82%A2

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {};
+  },
+  getters: {}, // like computed properties
+  mutations: {}, // like methods
+});
+
+export default store;
